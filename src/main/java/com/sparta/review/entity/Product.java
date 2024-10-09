a6dcf03dd10203dc07d132beaa15ab5d9ed7ee1e
NarooSister <dongeun49@naver.com>
2024-10-09 16:28:56 +0900

    [Feat] Product, Review entity 작성

---
+++ src/main/java/com/sparta/review/entity/Product.java
@@ -0,0 +1,18 @@
+package com.sparta.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long reviewCount;
+    private Double score;
+}
---
+++ src/main/java/com/sparta/review/entity/Review.java
@@ -0,0 +1,33 @@
+package com.sparta.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer score;
+
+    private String imageUrl;
+
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
